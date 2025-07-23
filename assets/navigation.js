1fa0d7c5fecc9f9a311cac139cf72c7d9ceae523
streamich <streamich@users.noreply.github.com>
2025-07-23 08:04:45 +0000

    deploy: 7e7c25fb4b4f4f62bb55eca6f07e9dc82b1ea329

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslLKyU8PKUpNVdJRKkgsyVCyUkorzUsuyczPK9aHSulllOTmKOkoZWfmpShZmZnU6sA1l+T7ZOZh1QuRwasVl7UQGQytsQAYbvWCtAAAAA=="
